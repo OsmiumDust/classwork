ff84ebfb571a7775f301de0a5f7a06450988c96f
OsmiumDust <38886849+OsmiumDust@users.noreply.github.com>
2019-03-19 13:12:45 -0400

    Add files via upload

--- Drawing with Loops.py
+++ Drawing with Loops.py
@@ -1,5 +1,5 @@
 import arcade
-
+import math
 
 WIDTH = 640
 HEIGHT = 480
@@ -12,15 +12,26 @@ def update(delta_time):
 def on_draw():
     arcade.start_render()
     # Draw in here...
-    rec_x, rec_y, rec_width, rec_length = 100, 100, 40, 80
-    tri_x1, tri_x2, tri_x3, tri_y1, tri_y2, tri_y3 = 120, 70, 170, 250, 180, 180
-    for tree in range(5):
+    def placement(x, y, end, space):
+        for tree in range(x, end):
+            if x <= end:
+                arcade.draw_xywh_rectangle_filled(x, y, 10, 30, arcade.color.GREEN)
+                arcade.draw_triangle_filled(x-10,y+30,x+20,y+30,x+5,y+100, arcade.color.BROWN)
+            x += space
+
+    placement(1, 25, WIDTH, 25)
+    placement(2, 125, WIDTH, math.pi)
+    placement(3, 250, WIDTH, 50)
+
+    '''rec_x, rec_y, rec_width, rec_length = 50, 50, 40, 80
+    tri_x1, tri_x2, tri_x3, tri_y1, tri_y2, tri_y3 = 50+20, 50+20-50, 50+20+50, 50+80+70, 50+80, 50+80
+    for tree in range(6):
         arcade.draw_xywh_rectangle_filled(rec_x, rec_y, rec_width, rec_length, arcade.color.GREEN)
         arcade.draw_triangle_filled(tri_x1, tri_y1, tri_x2, tri_y2, tri_x3, tri_y3, arcade.color.BROWN)
         rec_x += 100
         tri_x1 += 100
         tri_x2 += 100
-        tri_x3 += 100
+        tri_x3 += 100'''
 
 
 
