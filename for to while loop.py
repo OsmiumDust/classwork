491da103105ea5d9a15f543b4c8ca2647139cb79
OsmiumDust <38886849+OsmiumDust@users.noreply.github.com>
2019-03-26 13:22:12 -0400

    Create for to while loop.py

---
+++ for to while loop.py
@@ -0,0 +1,48 @@
+# 1
+my_str = "hello"
+i = 0
+while i < len(my_str):
+    """For every character in the string my_str, print the character"""
+    char = my_str[i]
+    print(char)
+    i += 1
+
+
+# 2
+numbers = [7, 7, 2, 7, 11]
+i = 0
+while i < len(numbers):
+    """For every number in the list of numbers, print the number."""
+    num = numbers[i]
+    print(num)
+    i += 1
+
+# 3
+marks = [84, 89, 90, 45, 67]
+i = 0
+while i < len(marks):
+    """For every mark in the list of marks, print the mark"""
+    mark = marks[i]
+    print(mark)
+    i += 1
+
+# 4
+l = range(10)
+i = 0
+while i != l:
+#For every number in the range, print the number
+  print(i)
+  i += 1
+
+# 5
+
+i = 5
+while i != 100:
+  print(i)
+  i += 5
+
+# 6
+i = 100
+while i > 0:
+  print(i)
+  i -= 5
